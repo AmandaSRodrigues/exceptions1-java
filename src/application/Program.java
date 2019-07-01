7cbe2b4aa2439129a6ccefb42d89d1bb80f60b08
Amanda Santos <amanda16santos@outlook.com>
2019-07-01 16:28:26 -0300

    Solução ruim

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,35 +13,41 @@ public class Program {
 
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
-		System.out.println("Room number: ");
+
+		System.out.print("Room number: ");
 		int number = sc.nextInt();
-		System.out.println("Check-in dade (dd/MM/yyyy: ");
+		System.out.print("Check-in date (dd/MM/yyyy): ");
 		Date checkIn = sdf.parse(sc.next());
-		System.out.println("Check-out dade (dd/MM/yyyy: ");
+		System.out.print("Check-out date (dd/MM/yyyy): ");
 		Date checkOut = sdf.parse(sc.next());
-		
+
 		if (!checkOut.after(checkIn)) {
-			System.out.println("Error in reservation: check-ut date must be after check-in date");
+			System.out.println("Error in reservation: Check-out date must be after check-in date");
 		}
 		else {
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
-			System.out.println(reservation);
-		
+			System.out.println("Reservation: " + reservation);
+
 			System.out.println();
-			System.out.println("Enter data to updae the reservation:");
-		
-			System.out.println("Room number: ");
-			number = sc.nextInt();
-			System.out.println("Check-in dade (dd/MM/yyyy: ");
+			System.out.println("Enter data to update the reservation:");
+			System.out.print("Check-in date (dd/MM/yyyy): ");
 			checkIn = sdf.parse(sc.next());
-			System.out.println("Check-out dade (dd/MM/yyyy: ");
+			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
-		
-			reservation.updateDates(checkIn, checkOut);
-			System.out.println("Reservation:" + reservation);
+
+			Date now = new Date();
+			if (checkIn.before(now) || checkOut.before(now)) {
+				System.out.println("Error in reservation: Reservation dates for update must be future dates");
+			}
+			else if (!checkOut.after(checkIn)) {
+				System.out.println("Error in reservation: Check-out date must be after check-in date");
+			}
+			else {
+				reservation.updateDates(checkIn, checkOut);
+				System.out.println("Reservation: " + reservation);
+			}
 		}
+
 		sc.close();
 	}
-
 }
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -5,15 +5,12 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Reservation {
-	
+
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
-	
+
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	
-	public Reservation() {
-	}
 
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
 		this.roomNumber = roomNumber;
@@ -41,25 +38,22 @@ public class Reservation {
 		long diff = checkOut.getTime() - checkIn.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	
+
 	public void updateDates(Date checkIn, Date checkOut) {
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Room "
-				+ roomNumber
-				+ ", check-in: "
-				+ sdf.format(checkIn)
-				+ ", check-out: "
-				+ sdf.format(checkOut)
-				+ ", "
-				+ duration()
-				+ "nigths";
+			+ roomNumber
+			+ ", check-in: "
+			+ sdf.format(checkIn)
+			+ ", check-out: "
+			+ sdf.format(checkOut)
+			+ ", "
+			+ duration()
+			+ " nights";
 	}
-	
-	
-
 }
